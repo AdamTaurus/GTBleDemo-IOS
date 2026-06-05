ef85455831371a24c054332c9274eb03a1198538
chengzheng <chengzheng@example.com>
2026-06-05 15:15:15 +0800

    Initial Commit

---
+++ GDBleDemo-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GDBleDemo-iOS
+//
+//  Created by 盛政 on 2026/6/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GDBleDemo-iOS/GDBleDemo_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GDBleDemo_iOSApp.swift
+//  GDBleDemo-iOS
+//
+//  Created by 盛政 on 2026/6/5.
+//
+
+import SwiftUI
+
+@main
+struct GDBleDemo_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
